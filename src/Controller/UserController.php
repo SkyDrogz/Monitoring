976fbd27ebee98189746f20f9a581062ba92edca
SkyDrogz <timothee.nitharum@gmail.com>
2018-01-16 16:30:52 +0100

    Modification finis

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -40,25 +40,25 @@ class UserController extends Controller
         return $this->render('user/new.html.twig', array('form' =>$form->createView()));
     }
     /**
-     * @Route("/user/edit", name="user_edit")
+     * @Route("/user/edit/{id}", name="user_edit")
      */
-    // public function editAction(Request $request,User $user)
-    // {
-    //     $em = $this->getDoctrine()->getManager();
-    //     $user= $em->getRepository('monitoring:User')->get_current_user();
-    //     $form = $this->createForm(UserType::class, $user);
-    //     $form->handleRequest($request);
-    //     //Submit
-    //     if($form->isSubmitted() && $form->isValid()){
-    //         $user = $form -> getData();
-    //         $em = $this->getDoctrine()->getManager();
-    //         $em -> persist($user);
-    //         $em->flush();
-    //         return $this->redirectToRoute('user_edit');
-    //     }
-    //     return $this->render('user/edit.html.twig', array('form' =>$form->createView()));
-    // }
-    //
+    public function edit(Request $request, $id)
+    {
+      // Création du formulaire d'édition d'un système
+      $em = $this->getDoctrine()->getManager();
+      $user = $em->getRepository(User::class)->find($id);
+      $form = $this->createForm(UserType::class, $user);
+      $form->handleRequest($request);
+
+      if($form->isSubmitted() && $form->isValid()){
+               $user = $form -> getData();
+               $em = $this->getDoctrine()->getManager();
+               $em -> persist($user);
+               $em->flush();
+               return $this->redirectToRoute('user');
+      }
+      return $this->render('user/edit.html.twig', array('form' =>$form->createView()));
+    }
     /**
      * @Route("/user/consultation", name="user_consultation")
      */
