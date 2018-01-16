56afdb0eab4f485fd7ea26c1c9d034d1dc356b1d
SkyDrogz <timothee.nitharum@gmail.com>
2018-01-16 11:58:26 +0100

    Parti new user presques finis

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,12 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends Controller
 {
+
     /**
      * @Route("/user", name="user")
      */
@@ -21,10 +27,47 @@ class UserController extends Controller
     /**
      * @Route("/user/new", name="user_new")
      */
-    public function new()
+    public function newAction(Request $request)
     {
-        // exit;
-        // replace this line with your own code!
-        return $this->render('user/new.html.twig');
+        $user = new User();
+        
+        $form = $this->createFormBuilder($user)
+            -> add ('identifiant', TextType::class)
+            -> add ('password', TextType::class)
+            -> add ('role', TextType::class)
+            -> add ('save', SubmitType::class, array('label'=>'Création utilisateur'))
+            -> getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $user = $form -> getData();
+            $em = $this->getDoctrine()->getManager();
+            $em -> persist($user);
+            $em->flush();
+            return $this->redirectToRoute('user_new');
+        }
+        return $this->render('user/new.html.twig', array('form' =>$form->createView()));
+    }
+    /**
+     * @Route("/user/edit", name="user_edit")
+     */
+    public function editAction()
+    {
+        exit;
+    }
+    /**
+     * @Route("/user/consultation", name="user_consultation")
+     */
+    public function consultationAction()
+    {
+        exit;
+    }
+    /**
+     * @Route("/user/suppression", name="user_suppression")
+     */
+    public function suppressionAction()
+    {
+        exit;
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Role;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
@@ -28,13 +29,56 @@ class User
      */
     private $role;
     /**
-     * @ORM\Column(type="dateTime")
+     * @ORM\Column(type="date")
      */
     private $dateConnexion;
     /**
-     * @ORM\Column(type="dateTime")
+     * @ORM\Column(type="date")
      */
     private $dateDeconnexion;
     
-    // add your own field
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getIdentifiant()
+    {
+        return $this->identifiant;
+    }
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    public function getRole()
+    {
+        return $this->role;
+    }
+    public function getDateConnexion()
+    {
+        return $this->dateConnexion;
+    }
+    public function getDateDeconnexion()
+    {
+        return $this->dateDeconnexion;
+    }
+    public function setIdentifiant($identifiant)
+    {
+         $this->identifiant = $identifiant;
+    }
+    public function setPassword($password)
+    {
+         $this->password = $password;
+    }
+    public function setRole($role)
+    {
+         $this->role = $role;
+    }
+    public function setdateConnexion($dateConnexion)
+    {
+         $this->dateConnexion = $dateConnexion;
+    }
+    public function setDateDeconnexion($dateDeconnexion)
+    {
+         $this->dateDeconnexion = $dateDeconnexion;
+    }
 }
