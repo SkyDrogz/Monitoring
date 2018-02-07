9e869cee150207120368d6396f3cfe8e9fa5ac0a
SkyDrogz <timothee.nitharum@gmail.com>
2018-02-07 16:42:33 +0100

    fix #4

--- tests/Controller/EntrepriseControllerTest.php
+++ tests/Controller/EntrepriseControllerTest.php
@@ -22,7 +22,6 @@ class EntrepriseControllerTest extends WebTestCase
             'PHP_AUTH_PW' => 'admin',
         ));
         $crawler = $client->request('GET', '/entreprise/new');
-        // echo $crawler -> html();
         $form = $crawler->selectButton("Confirmer l'ajout")->form();
 
         $form['entreprise[libelle]'] = 'Richard';
@@ -39,7 +38,6 @@ class EntrepriseControllerTest extends WebTestCase
             'PHP_AUTH_PW' => 'admin',
         ));
         $crawler = $client->request('GET', '/entreprise/edit/4');
-        // echo $crawler -> html();
         $form = $crawler->selectButton("Modification")->form();
 
         $form['entreprise[libelle]'] = 'Richou';
@@ -51,30 +49,24 @@ class EntrepriseControllerTest extends WebTestCase
     }
     public function testSuppression()
     {
-        // dump($user);exit;
 
         $client = static::createClient(array(), array(
             'PHP_AUTH_USER' => 'Baptiste',
             'PHP_AUTH_PW' => 'admin',
         ));
-        // $user->setIdentifiant('Richard');
 
         $crawler = $client->request('GET', '/entreprise/delete/4');
-        // echo $crawler -> html();
         $this->assertTrue(true, $client->getResponse()->isRedirect('entreprise/read'));
     }
     public function testEntrepriseReactivation()
     {
-        // dump($user);exit;
 
         $client = static::createClient(array(), array(
             'PHP_AUTH_USER' => 'Baptiste',
             'PHP_AUTH_PW' => 'admin',
         ));
-        // $user->setIdentifiant('Richard');
 
         $crawler = $client->request('GET', '/entreprise/reactive/4');
-        // echo $crawler -> html();
         $this->assertTrue(true, $client->getResponse()->isRedirect('entreprise/active'));
     }
 }
